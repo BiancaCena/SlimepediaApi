d231cd24bf9735525225c0c6ba1ff08fdf143eee
Bianca Cena <bibcena21@gmail.com>
2024-08-28 16:40:37 +0800

    refactor: update slime schema to use constants

--- models/slimeModel.js
+++ models/slimeModel.js
@@ -1,135 +1,100 @@
 const mongoose = require("mongoose");
+const {
+	DIETS,
+	FAVORITE_TOYS,
+	FAVORITE_FOODS,
+	SLIME_TYPES,
+	SPAWN_LOCATIONS,
+	GAME_IDS,
+} = require("../constants/slimeConstants");
 
-const slimeSchema = new mongoose.Schema(
-	{
-		_id: String,
-		name: {
-			type: String,
-			required: [true, "A slime must have a name"],
-			trim: true,
-			maxlength: [30, "A slime name must be 30 characters or fewer"],
-			minlength: [3, "A slime name must be at least 3 characters"],
-		},
-		image: {
-			type: String,
-			trim: true,
-			maxlength: [255, "An image URL must be 255 characters or fewer"],
-		},
-		diet: {
-			type: String,
-			required: [true, "A slime must have a diet"],
-			enum: {
-				values: [
-					"all",
-					"meat",
-					"veggie",
-					"fruit",
-					"water",
-					"ash",
-					"nectar",
-					"other",
-				],
-				message:
-					"Diet must be one of the following: all, meat, veggie, fruit, water, ash, nectar, other",
-			},
-		},
-		favouriteToy: {
-			type: String,
-			enum: {
-				values: [
-					"beachball",
-					"yarn",
-					"bigrock",
-					"nightlight",
-					"bee",
-					"ducky",
-					"cell",
-					"octo",
-					"bomb",
-					"chicken",
-					"crystalball",
-					"puzzlecube",
-					"gyro",
-					"brick",
-					"solmate",
-					"disco",
-					"stegobuddy",
-				],
-				message:
-					"Favorite toy must be one of the following: Beach Ball, Yarn Ball, Big Rock, Night Light, Buzzy Bee, Rubber Ducky, Power Cell, Octo Buddy, Bomb Ball, Stuffed Chicken, Crystal Ball, Puzzle Cube, Gyro Top, Charcoal Brick, Sol Mate, Disco Ball, Stego Buddy",
-				default: "none",
-			},
-		},
-		favouriteFood: {
-			type: String,
-			enum: [
-				"none",
-				"ranchers",
-				"roostro",
-				"elderroostro",
-				"hen",
-				"elderhen",
-				"stonyhen",
-				"briarhen",
-				"paintedhen",
-				"chickadoo",
-				"stonychickadoo",
-				"briarchickadoo",
-				"paintedchickadoo",
-				"pogo",
-				"carrot",
-				"beet",
-				"cuberry",
-				"mango",
-				"oca",
-				"onion",
-				"lemon",
-				"pear",
-				"parsnip",
-				"ginger",
-				"kookadoba",
-				"lettuce",
-				"moondewnectar",
-				"pomegranite",
-				"seahen",
-				"seachickadoo",
-			],
-			message:
-				"Favorite food must be one of the following: Ranchers, Roostro, Elder Roostro, Hen Hen, Elder Hen, Stony Hen, Briar Hen, Painted Hen, Chickadoo, Stony Chickadoo, Briar Chickadoo, Painted Chickadoo, Pogo Fruit, Carrot, Heart Beet, Cuberry, Mint Mango, Oca Oca, Odd Onion, Quantum Lemon, Prickle Pear, Silver Parsnip, Gilded Ginger, Kookadoba, Water Lettuce, Moondew Nectar, Pomegranite, Sea Hen, Sea Chickadoo",
-			default: "none",
-		},
-		type: {
-			type: String,
-			required: [true, "A slime must have a type"],
-			enum: {
-				values: ["docile", "harmful", "hostile", "special"],
-				message:
-					"Type must be one of the following: docile, harmful, hostile, special",
-			},
-		},
-		slimepedia: {
-			slimeology: { type: String, trim: true, default: "" },
-			risks: { type: String, trim: true, default: "" },
-			plortonomics: { type: String, trim: true, default: "" },
-		},
-		locations: {
-			type: [String],
-			default: [],
-		},
-		properties: {
-			type: [String],
-			default: [],
-		},
-		games: {
-			type: [Number],
-			default: [],
+const slimeSchema = new mongoose.Schema({
+	_id: String,
+	name: {
+		type: String,
+		required: [true, "A slime must have a name"],
+		trim: true,
+		maxlength: [30, "A slime name must be 30 characters or fewer"],
+		minlength: [3, "A slime name must be at least 3 characters"],
+	},
+	image: {
+		type: String,
+		trim: true,
+		maxlength: [255, "An image URL must be 255 characters or fewer"],
+	},
+	diet: {
+		type: String,
+		required: [true, "A slime must have a diet"],
+		enum: {
+			values: DIETS,
+			message: `Diet must be one of the following: ${DIETS.join(", ")}`,
 		},
 	},
-	{
-		toJSON: { virtuals: true },
-		toObject: { virtuals: true },
-	}
-);
+	favouriteToy: {
+		type: String,
+		enum: {
+			values: FAVORITE_TOYS,
+			message: `Favorite toy must be one of the following: ${FAVORITE_TOYS.join(
+				", "
+			)}`,
+			default: "none",
+		},
+	},
+	favouriteFood: {
+		type: String,
+		enum: {
+			values: FAVORITE_FOODS,
+			message: `Favorite food must be one of the following: ${FAVORITE_FOODS.join(
+				", "
+			)}`,
+			default: "none",
+		},
+	},
+	type: {
+		type: String,
+		required: [true, "A slime must have a type"],
+		enum: {
+			values: SLIME_TYPES,
+			message: `Type must be one of the following: ${SLIME_TYPES.join(", ")}`,
+		},
+	},
+	slimepedia: {
+		slimeology: { type: String, trim: true, default: "" },
+		risks: { type: String, trim: true, default: "" },
+		plortonomics: { type: String, trim: true, default: "" },
+	},
+	locations: {
+		type: [String],
+		enum: {
+			values: SPAWN_LOCATIONS,
+			message: `Spawn location/s must be one of the following: ${SPAWN_LOCATIONS.join(
+				", "
+			)}`,
+		},
+		default: [],
+	},
+	properties: {
+		type: [String],
+		default: [],
+	},
+	games: {
+		type: [Number],
+		// Custom validation to ensure that each game ID provided is valid
+		validate: {
+			validator: function (value) {
+				// Allow empty array or ensure all elements are valid game IDs
+				return (
+					// Validates empty array
+					value.length === 0 ||
+					// Check if every number in the array is included in GAME_IDS
+					value.every((gameId) => GAME_IDS.includes(gameId))
+				);
+			},
+			message: `Game/s must be one of the following: ${GAME_IDS.join(", ")}`,
+		},
+		default: [],
+	},
+});
 
 // Create model based on schema
 const Slime = mongoose.model("Slime", slimeSchema);
