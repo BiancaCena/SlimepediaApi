8ebc70c4f1521679cd586e508ecb3013e6ed3394
Bianca Cena <bibcena21@gmail.com>
2024-08-28 16:40:00 +0800

    refactor: create constants for slime schema

---
+++ constants/slimeConstants.js
@@ -0,0 +1,98 @@
+// Diet types
+const DIETS = [
+	"all",
+	"meat",
+	"veggie",
+	"fruit",
+	"water",
+	"ash",
+	"nectar",
+	"other",
+];
+
+// Favorite toys
+const FAVORITE_TOYS = [
+	"none",
+	"beachball",
+	"yarn",
+	"bigrock",
+	"nightlight",
+	"bee",
+	"ducky",
+	"cell",
+	"octo",
+	"bomb",
+	"chicken",
+	"crystalball",
+	"puzzlecube",
+	"gyro",
+	"brick",
+	"solmate",
+	"disco",
+	"stegobuddy",
+];
+
+// Favorite foods
+const FAVORITE_FOODS = [
+	"none",
+	"ranchers",
+	"roostro",
+	"elderroostro",
+	"hen",
+	"elderhen",
+	"stonyhen",
+	"briarhen",
+	"paintedhen",
+	"chickadoo",
+	"stonychickadoo",
+	"briarchickadoo",
+	"paintedchickadoo",
+	"pogo",
+	"carrot",
+	"beet",
+	"cuberry",
+	"mango",
+	"oca",
+	"onion",
+	"lemon",
+	"pear",
+	"parsnip",
+	"ginger",
+	"kookadoba",
+	"lettuce",
+	"moondewnectar",
+	"pomegranite",
+	"seahen",
+	"seachickadoo",
+];
+
+// Slime types
+const SLIME_TYPES = ["docile", "harmful", "hostile", "special"];
+
+// Slime locations
+const SPAWN_LOCATIONS = [
+	"blanket",
+	"bluffs",
+	"desert",
+	"fields",
+	"nimble",
+	"quarry",
+	"reef",
+	"ruins",
+	"slimeulation",
+	"strand",
+	"valley",
+	"wilds",
+];
+
+// Slime rancher games
+const GAME_IDS = [1, 2];
+
+module.exports = {
+	DIETS,
+	FAVORITE_TOYS,
+	FAVORITE_FOODS,
+	SLIME_TYPES,
+	SPAWN_LOCATIONS,
+	GAME_IDS,
+};
