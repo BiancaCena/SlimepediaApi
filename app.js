ab0a2c661c3fa90143e0125203ff9ff6698de5f1
Bianca Cena <bibcena21@gmail.com>
2024-08-30 21:11:23 +0800

    feat: add rate limit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const path = require("path");
 
 // import routes
@@ -13,6 +14,14 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
+
+// Allow 100 requests from same IP in one hour
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: "Too many requests from this IP, please try again in an hour.",
+});
+app.use("/api", limiter);
 
 // Middleware to parse JSON bodies
 app.use(express.json());
